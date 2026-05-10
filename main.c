982ece5e87be8843d7ded78373cf0f39d58aa08c
Rogério Freitas <rogerio.rdaf@gmail.com>
2026-05-10 17:25:28 -0300

    feat: add deletePerson function

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 void* addPerson(void *pBuffer);
 void* listPeople(void *pBuffer);
 void* searchPerson(void *pBuffer);
+void* deletePerson(void *pBuffer);
 
 
 
@@ -36,7 +37,7 @@ int main(){
             pBuffer = addPerson(pBuffer);
             break;
             case 2:
-            /* code */
+            pBuffer = deletePerson(pBuffer);
             break;
             case 3:
             pBuffer = searchPerson(pBuffer);
@@ -84,7 +85,7 @@ void* addPerson(void* pBuffer){
     return pBuffer;
 }
 
-void* listPeople(void *pBuffer){
+void* listPeople(void* pBuffer){
     *(int*)((char *)pBuffer + 2 * sizeof(int)) = 0;
 
     while(*(int*)((char *)pBuffer + 2 * sizeof(int)) < *(int*)((char *)pBuffer + sizeof(int))){
@@ -102,9 +103,11 @@ void* listPeople(void *pBuffer){
 
         printf("-----------------------------\n");
     }
+
+    return pBuffer;
 }
 
-void* searchPerson(void * pBuffer){
+void* searchPerson(void* pBuffer){
     *(int*)((char *)pBuffer + 2 * sizeof(int)) = 0;
 
     if (*(int*)((char *)pBuffer + sizeof(int)) == 0)
@@ -150,4 +153,84 @@ void* searchPerson(void * pBuffer){
     }
 
     return pBuffer;
+}
+
+void* deletePerson(void* pBuffer){
+*(int*)((char *)pBuffer + 2 * sizeof(int)) = 0;
+
+if (*(int*)((char *)pBuffer + sizeof(int)) == 0)
+{
+    printf("Primeiro cadastre ao menos um usuário para poder deletar!");
+    return pBuffer;
+}
+
+
+printf("\nDigite o nome de quem você quer deletar:\n");
+fgets((char *)pBuffer + 3 * sizeof(int), 100, stdin);
+((char *)pBuffer + 3 * sizeof(int))[strcspn((char *)pBuffer + 3 * sizeof(int), "\n")] = '\0';
+
+while(*(int*)((char *)pBuffer + 2 * sizeof(int)) < *(int*)((char *)pBuffer + sizeof(int))){
+
+    if(strcmp((char *)pBuffer + 3 * sizeof(int), (char *)pBuffer + 3 * sizeof(int) + 100 + *(int *)((char *)pBuffer + 2 * sizeof(int))) == 0){
+            //IMPRIMIR NOME
+        printf("\nNome: %s\n", (char*)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int)));
+        *(int*)((char *)pBuffer + 3 * sizeof(int)) = *(int *)((char *)pBuffer + 2 * sizeof(int));
+        *(int *)((char *)pBuffer + 2 * sizeof(int)) += strlen((char*)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int))) + 1;
+
+        //IMPRIMIR IDADE
+        printf("Idade: %d\n",*(int*)((char *)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int))));
+        *(int *)((char *)pBuffer + 2 * sizeof(int)) += sizeof(int);
+
+        //IMPRIMIR EMAIL
+        printf("Email: %s\n", (char *)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int)));
+        *(int*)((char *)pBuffer + 2 * sizeof(int)) += strlen((char *)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int))) + 1;
+
+        printf("-----------------------------\n");
+        printf("Esse é o cadastro que deseja apagar?\n 1 - Sim\n 2 - Não\n->");
+
+        scanf("%d", (int*)pBuffer);
+        getchar();
+        if(*(int*)pBuffer == 1){
+            
+            memmove(
+                ((char *)pBuffer + 3 * sizeof(int)) + 100 + *(int *)((char *)pBuffer + 3 * sizeof(int)),
+                ((char *)pBuffer + 3 * sizeof(int)) + 100 + *(int *)((char *)pBuffer + 2 * sizeof(int)),
+                *(int *)((char *)pBuffer + sizeof(int)) - *(int *)((char *)pBuffer + 2 * sizeof(int))
+            );
+
+            *(int*)((char *)pBuffer + sizeof(int)) = *(int*)((char *)pBuffer + sizeof(int)) - (*(int *)((char *)pBuffer + 2 * sizeof(int)) - *(int *)((char *)pBuffer + 3 * sizeof(int)));
+
+            pBuffer = realloc(pBuffer, 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + sizeof(int)));
+
+            *(int*)((char *)pBuffer + 2 * sizeof(int)) = 0-*(int*)((char *)pBuffer + 2 * sizeof(int))-1;
+            break;
+
+        }else if(*(int*)pBuffer == 2){
+            printf("\nRetornando para o Menu principal.\n");
+            *(int*)((char *)pBuffer + 2 * sizeof(int)) = 0-*(int*)((char *)pBuffer + 2 * sizeof(int));
+            break;
+        }else{
+            printf("\nOpção inválida retornando ao menu principal...\n");
+            *(int*)((char *)pBuffer + 2 * sizeof(int)) = 0-*(int*)((char *)pBuffer + 2 * sizeof(int));
+            break;
+        }
+
+
+    }
+
+    *(int *)((char *)pBuffer + 2 * sizeof(int)) += strlen((char*)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int))) + 1;
+    *(int *)((char *)pBuffer + 2 * sizeof(int)) += sizeof(int);
+    *(int*)((char *)pBuffer + 2 * sizeof(int)) += strlen((char *)pBuffer + 3 * sizeof(int) + 100 + *(int*)((char *)pBuffer + 2 * sizeof(int))) + 1;
+
+    
+}
+if(*(int*)((char *)pBuffer + 2 * sizeof(int)) > 0){
+    printf("\nNome não encontrado!\nRetornando ao menu......\n");
+}else if(*(int*)((char *)pBuffer + 2 * sizeof(int)) < 0){
+    printf("\nNome deletado com sucesso!\n Retornando ao menu.....\n");
+}
+*(int*)pBuffer = 0;
+return pBuffer; 
+
+
 }
